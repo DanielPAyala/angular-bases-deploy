9b507d7ac3c2514a1d007f446bd8cc1f638955f3
Carlos Daniel <carlosporrasayala@gmail.com>
2024-02-09 23:56:37 -0500

    Add Character list and a Character interface

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 8500,
+    },
+  ];
+}
